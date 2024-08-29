38cc5e26539114c13290ca3d50c5c318c4865523
miktaba <mtabakaev@gmail.com>
2024-08-29 11:57:18 +0200

    Created a prototype cell
    Created a prototype cell with using Horizontal Stack
    View and custom class

---
+++ SimpleTo-DoManager/SimpleTo-DoManager/View/Cels/TaskCellTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TaskCellTableViewCell.swift
+//  SimpleTo-DoManager
+//
+//  Created by Mikhail Tabakaev on 8/29/24.
+//
+
+import UIKit
+
+class TaskCellTableViewCell: UITableViewCell {
+    
+    @IBOutlet var symbol: UILabel!
+    @IBOutlet var title: UILabel!
+
+}
